3b2fe09528de944ec4049d027c0780018225f84f
Howard Lucas <h@bylucas.co.uk>
2020-10-14 19:01:22 +0100

    oct20

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const app = express();
 const dbURI = "mongodb+srv://bylucas:Bear02Head@blog.3ni8v.mongodb.net/blog?retryWrites=true&w=majority";
 
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(result => app.listen())//3000
+  .then(result => app.listen(3000))
   .catch(err => console.log(err));
 
 // register view engine
